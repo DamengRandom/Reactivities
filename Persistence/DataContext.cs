10db7c37cae3b07229dbe78158b031294fa9b4d0
damon.wu <damon.wu@myob.com>
2020-09-13 10:36:10 +1000

    initial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {}
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value {Id = 1, Name = "Damon"},
+                    new Value {Id = 2, Name = "Ella"},
+                    new Value {Id = 3, Name = "Unknown"}
+                );
+        } // create mock data
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,11 @@
+## Welcome to Project of C# Practice
+
+
+### Project structure
+
+Persistence has 1 reference: Domain
+Application has 2 references: Domain + Persistence
+API has 1 reference: Application
+
+
+###
